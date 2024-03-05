23859c83b5b1b38ce97df7ef6c167ee7ca8b91b1
MartinFernandezSantia <martin-santia@hotmail.com.ar>
2024-03-05 12:21:23 -0300

    funciones y clases

---
+++ python.py
@@ -0,0 +1,50 @@
+# if 1==2:
+#     print(1)
+# elif True==1:
+#     print(2)
+# else:
+#     print("hola")
+
+diccionarios = {
+    'Matias': 'puto',
+    'Juan': 2,
+    2: 3
+}
+
+listas = [1,2,3,4, "hola", diccionarios, {'Hola': 'chau'}]
+# print(listas[5]['Matias'])
+
+# print(diccionarios[2])}
+
+for i in range(len(listas)):
+    print(listas[i])
+
+contador = 0
+while contador < len(listas):
+    print(listas[contador])
+    contador += 1
+
+# int sumar(int a, int b){
+
+# }
+    
+def sumar(a, b, c = 100):
+    return (a + b) * c
+
+print(sumar(1, 2, 4))
+
+
+class Usuario:
+    def __init__(self, nombre, edad, mail):
+        self.nombre = nombre
+        self.edad = edad
+
+        self.saludar()
+        return
+    
+    def saludar(self):
+        print(f'Hola soy {self.nombre} y tengo {self.edad}')
+
+user = Usuario('Martin', 20, 'asd@asd.com')
+
+user2 = Usuario('Matias', 20, 'aioijw@oiajd.com')
